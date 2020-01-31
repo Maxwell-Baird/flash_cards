5d60876a21c77f81c58d6a5936b87291b6da062b
Maxwell Baird <bairdm71@gmail.com>
2020-01-31 08:51:20 -0700

    Added a user interface

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -12,22 +12,38 @@ card4 = Card.new("Who was the first governor of Wyoming?", "John Campbell", :His
 deck = Deck.new([card1, card2, card3, card4])
 round = Round.new(deck)
 
+
 def start(round_parameter)
+  count = 1
+  category_holder = []
   puts "Welcome! You're playing with #{round_parameter.deck.count} cards"
   puts "-" * 50
   round_parameter.deck.cards.each do |card|
-    count = 1
     puts "This is card number #{count} out of #{round_parameter.deck.count}"
     puts "Question: #{card.question}"
     puts "What is your guess"
     input = gets.chomp
-
+    category_holder << card.category
     turn = round_parameter.take_turn(input)
     puts turn.feedback
     count += 1
   end
   puts "********* GAME OVER *********"
   puts "You had #{round_parameter.number_correct} out of #{round_parameter.deck.count} for total score of #{round_parameter.percent_correct}%"
+
+  already_shown = [:junk]
+  category_holder.each do |percent_print|
+    has_shown = false
+    already_shown.each do |check|
+      if check == percent_print
+        has_shown = true
+      end
+    end
+    if has_shown == false
+      puts "#{percent_print} - #{round_parameter.percent_correct_by_category(percent_print)}% correct"
+      already_shown << percent_print
+    end
+  end
 end
 
 start(round)
